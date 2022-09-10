3ab1367724276bb56b18b395dd19910f915e84b8
Priyanka Mishra <prriyamishra44@gmail.com>
2022-09-10 23:28:47 +0530

    changes in welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,11 +3,11 @@
 import React from 'react'
 
 export default function Welcome(props) {
-    
+    let message = `Welcome to Newton School.`
   return (
     <div>
       <h1>Hey ! {props.name}</h1>
-      <h2>Welcome to Newton School.</h2>
+      <h2>{message}</h2>
     </div>
   )
 }
