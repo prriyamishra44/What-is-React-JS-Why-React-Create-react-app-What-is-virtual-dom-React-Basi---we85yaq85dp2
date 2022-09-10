5ce5edbc5308bf14dcd22faa6ef01597fa164148
Priyanka Mishra <prriyamishra44@gmail.com>
2022-09-10 21:27:36 +0530

    changes done in file

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -7,7 +7,7 @@ export default function Welcome(props) {
   return (
     <div>
       <h1>Hey ! {props.name}</h1>
-      <h2>Welcome to Newton School</h2>
+      <h2>"Welcome to Newton School."</h2>
     </div>
   )
 }
